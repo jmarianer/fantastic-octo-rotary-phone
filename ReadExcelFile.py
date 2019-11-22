c59e6a38531cd9fd2c09da30098b1290181fb1ca
Marni <marni@joeym.org>
2019-11-22 03:56:37 +0000

    Ready for initial review.

---
+++ ReadExcelFile.py
@@ -0,0 +1,19 @@
+#This program will read in an Excel file and print the contents of a specific cell.
+
+#import openpyxl module function load_workbook
+from openpyxl import load_workbook
+
+#Define variable name for the workbook
+wb = load_workbook('samplebook.xlsx')
+
+#Define variable name for worksheet
+sheet = wb['Sheet1']
+
+#Define variable for the cell to read
+cell = sheet.cell(1,1)
+
+#Define varible for cell value to print
+my_val = cell.value
+
+#Print the cell value
+print(my_val)
