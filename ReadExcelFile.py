c53857a0e5a83bf577e868cfd2b9bff34d14f000
Marni <marni@joeym.org>
2019-11-24 16:04:14 +0000

    Added creating and printing a list.

--- ReadExcelFile.py
+++ ReadExcelFile.py
@@ -19,6 +19,15 @@ my_val = cell.value
 #Print the cell value
 print(my_val)
 
+#Read in values of cells C1 through C5 as list (array).
+Cells_List=[]
+for i in range(1,6):
+    Cells_List.append(sheet.cell(i,3))
+
+#Print the values of list Cells_List.
+for j in range(1,6):
+    print(Cells_List[j-1].value)
+
 #Read in values of cells C1 through C5 and print the total value.
 #Define the first value to be used as the base of the for loop.
 Total_Cell_Values = 0
@@ -28,7 +37,6 @@ for i in range(1,6):
     Total_Cell_Values = Total_Cell_Values + C_Cell_value
 
 #Write Total_Cell_Values to Excel file cell D1.
-print(Total_Cell_Values)
 sheet['D1'] = Total_Cell_Values
 
 wb.save("sample_output.xlsx")
